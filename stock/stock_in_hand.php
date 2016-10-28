9738ef4507b47dc5ebf6f9c6e4c021f21c6b9dbb
RukshiDeSilva <prramodidesilva45@gmail.com>
2016-10-28 07:11:44 +0530

    added file

---
+++ stock/stock_in_hand.php
@@ -0,0 +1,45 @@
+<?php
+
+
+
+function stock_in_hand($manufac,$sold){
+
+ $query= "SELECT battery_type, amount FROM released_batteries";
+$res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	?>
+
+
