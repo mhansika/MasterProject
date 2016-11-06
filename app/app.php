ee59549347ed7481ae853767b62660cee4ab4147
pmjproject <jayakodymangalya@gmail.com>
2016-11-06 19:58:26 +0530

    app

---
+++ app/app.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title></title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<style type="text/css">
+    .row > div{
+        margin-bottom: 10px;
+    }
+    .header{
+        min-height: 90px;
+
+    }
+    .footer{
+        min-height: 60px;
+    }
+    .header, .footer{
+        background: black;
+    }
+    .sidebar{
+        background: #dbdfe5;
+    }
+    .intro-left{
+        background: #b32400;
+        min-height: 250px;
+    }
+    .intro-right{
+        background: #e5ccff;
+        min-height: 250px;
+    }
+    .section{
+        min-height: 250px;
+        background: #404040;
+    }
+    .section.bg-alt{
+        background: #990000;
+        min-height: 250px;
+    }
+
+    body {
+    text-align:center;
+    font-family:sans-arial;
+    font-size: 20px;
+    font-weight: bold;
+    }
+
+    a {
+    color: black;
+    text-decoration: none;
+
+    }
+
+   .pic{
+    margin-top: 0px;
+    width: 145px;
+    }
+
+    h2{
+    margin-top:10px;
+    font-family:sans-arial;
+    font-size: 20px;
+    color: white;
+}
+
+h1  {
+    margin-top:;
+    font-family:sans-arial;
+    font-size: 12px;
+    color: white;
+
+}
+
+
+
+
+
+</style>
+</head>
+<body>
+
+
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="header"><img class="pic" src="img2/logo.png" align="left" height="80px" width="100px">
+                <a href="#"  style="display:block;float:right;margin-right:45px;margin-top:35px;color: white;font-size:18px;margin-bottom:10px;padding-bottom:10px;"> <img class="signout" src="img2/signout.png" >Signout </a>
+
+        <h2 style="font-weight: bolder" >ASSOCIATED BATTERY MANUFACTURERS (CEYLON) LIMITED</h1>
+        <h1 style="font-weight: bold;font-size: 20px">Warranty Management System</h2>
+    
+        
+    </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6">
+                <div class="intro-left"><a  href="newsold.php">
+                <span style="margin-top: -25%; margin-right: 55%">New Sold Batteries</span>
+                <img class= "pic" src="img2/m.png" align="middle" >
+                    
+        </a>
+
+        </div>
+   </div>
+            
+            <div class="col-xs-6">
+                <div class="intro-right"><a href="issuednew.php">
+                    <span style="margin-top: -25%; margin-right: 35%">Issued New Replacements</span>
+                    <img class= "pic" src="img2/a.png" align="middle">
+        </a>
+        </div>
+            </div>
+       
+        </div>
+        <div class="row">
+            <div class="col-xs-6">
+                <div class="section"><a href="replacement.php">
+                    <span style="margin-top: -25%; margin-right: 55%">Replacements</span>
+                    <img class= "pic" src="img2/c.png" align="middle">
+        </a>
+        </div>
+            </div>
+            <div class="col-xs-6">
+                <div class="section bg-alt"><a href="returninward.php">
+                    <span style="margin-top: -25%; margin-right: 55%">Return Inwards</span>
+                    <img class= "pic" src="img2/d.png" align="middle">
+        </a>
+        </div></div>
+            </div>
+        
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="footer"></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>                                      
