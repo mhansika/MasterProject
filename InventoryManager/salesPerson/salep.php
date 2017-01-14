2e40d1316ace85bdd7033246906f401e7796d033
senalishalika <cs4shalika@gmail.com>
2017-01-14 19:32:31 +0530

    salep view ok

--- InventoryManager/salesPerson/salep.php
+++ InventoryManager/salesPerson/salep.php
@@ -60,78 +60,76 @@ $role= $user_data['role'];
             <div class="form">
                 <div class="seperate">
                     <?php
-                    $servername = "localhost";
-                    $username = "root";
-                    $password = "";
-                    $dbname = "warranty_management";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "warranty_management";
 
-                    // Create connection
-                    $conn = mysqli_connect($servername, $username, $password, $dbname);
-                    // Check connection
-                    if (!$conn) {
-                        die("Connection failed: " . mysqli_connect_error());
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+//SELECT E.first_name NAME,D.department_name DNAME FROM employees E JOIN departments D ON (E.department_id = D.department_id);
+$sql = "SELECT S.* , A.area FROM sales_person S JOIN area A ON (S.area_no=A.area_no)";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<style>
+                    table {
+                        border-collapse: collapse;
+                        width: 80%;
+                        
                     }
-                    $sql = "SELECT salesPerson_id, F_name, L_name, area_no, NIC, address,DOB,mobileNo,telephoneNo,email FROM sales_person";
-                    $result = $conn->query($sql);
-                    if ($result->num_rows > 0) {
-                        echo "<head>
-                        <style>
-                            table {
-                                border-collapse: collapse;
-                                width: 100%;
-                                }
-                            th, td {
-                                text-align: left;
-                                padding: 8px;
-                                }
-                            tr:nth-child(even){
-                                background-color: #f2f2f2
-                                }
-                            
-                            th {
-                                background-color: #990000;
-                                color: white;
-                                }
-                            tbody {
-                                height: 100px;        
-                                }
-                            </style>
-                        </head>
-                        <body>
-                            <table>
-                            <tr>
-                            <th>ID</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Area </th>
-                            <th>NIC</th>
-                            <th>Address</th>
-                            <th>DOB</th>
-                            <th>Mobile No</th>
-                            <th>Tel No</th>
-                            <th>E mail</th>
-                            </tr>";
-                        // output data of each row
-                        while($row = $result->fetch_assoc()) {
-                            echo "<tr>
-                                        <td>".$row["salesPerson_id"]."</td>
-                                        <td>".$row["F_name"]." </td>
-                                        <td>".$row["L_name"]." </td>
-                                        <td>".$row["area_no"]." </td>
-                                        <td>".$row["NIC"]." </td>
-                                        <td>".$row["address"]." </td>
-                                        <td>".$row["DOB"]." </td>
-                                        <td>".$row["mobileNo"]." </td>
-                                        <td>".$row["telephoneNo"]." </td>
-                                        <td>".$row["email"]." </td> 
-                                    </tr>";
-                        }
-                        echo "</table></body></html>";
-                    } else {
-                        echo "0 results";
+                    th, td {
+                        text-align: center;
+                        padding: 8px;
                     }
-                    $conn->close();
-                    ?>
+                    tr:nth-child(even){background-color: #f2f2f2}
+                    th {
+                        background-color: #990000;
+                        color: white;
+                    }
+                    tbody {
+                        height: 100px;   
+                        overflow-y: auto;  
+                        overflow-x: hidden;  
+                    }
+                    .dealer{
+                        margin-left: 15%;
+                        padding-top: 5%;
+                        margin-top: 2%;
+                    }
+                    </style>
+            </head>
+            <body>
+            <table class='dealer'>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Area</th>
+                    <th>NIC</th>
+            
+                    <th>Address</th>
+                    <th>Mobile No</th>
+                    
+                    <th>E mail</th>
+                    
+                </tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $fname=$row['F_name'];
+        $lname=$row['L_name'];
+        $name=$fname.$lname;
+        echo "<tr><td>".$row["salesPerson_id"]."</td><td>".$name."</td><td>".$row["area"]."</td><td>".$row["NIC"]."</td><td>".$row["address"]."</td><td>".$row["mobileNo"]."</td><td>".$row["email"]."</td></tr>";
+    }
+    echo "</table></body></html>";
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
                 </div>
             </div>
 </div>
